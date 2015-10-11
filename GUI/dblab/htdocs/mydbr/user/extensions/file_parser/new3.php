7858a1f43f053f637c15789ac72131cd03d3bc33
Erez Maadani <erez.maadani@gmail.com>
2015-10-12 00:49:36 +0300

    use regex to split lines

--- GUI/dblab/htdocs/mydbr/user/extensions/file_parser/new3.php
+++ GUI/dblab/htdocs/mydbr/user/extensions/file_parser/new3.php
@@ -24,28 +24,34 @@ function read_text_file($fullPath){
 	if(!is_dir($dir)){
 		mkdir($dir);
 	}
+	$generatedScriptName = $dir . '/' . basename($fullPath) . '.sql';
+	
 	$text = getTextFileContents($fullPath);
 
-	$matches2 = preg_split('/$\R?^/m', $text['content']);
-
-	//print_r($matches2);
+	$matches2 = preg_split('/$\R?^/m', $text['content']);	
 
 	$indices = array_keys($matches2);
 
-	$format = 'insert into(%1$s, %2$s, %3$s, %4$s);' . PHP_EOL;
+	$format = 'insert into(%1$s, %2$s, %3$s, %4$s, %5$s);' . PHP_EOL;
 	
 	$numOfWords = 0;
-
+	$numOfWordsInLine = 0;
+	
 	foreach($indices as $index){
+		$numOfWordsInLine = 0;
 		echo 'Processing Line #' . ($index+1) . '<br/>';
-		$words = (preg_split('/\W/', $matches2[$index], -1, PREG_SPLIT_NO_EMPTY));
-		foreach($words as $word){
-			file_put_contents($dir . '/' . basename($fullPath) . '.sql', sprintf($format, $word, $index, $text['rhyme_scheme'], $text['auther']), FILE_APPEND);
-			$numOfWords++;
-		}	
+		$words = preg_split("/(?<=\w|\W)\b\s*/", $matches2[$index], -1, PREG_SPLIT_NO_EMPTY);
+		
+		print_r($words);
+		// foreach($words as $word){
+			// file_put_contents($generatedScriptName, sprintf($format, '"'. $word . '"', $index, $numOfWords,$numOfWordsInLine, ctype_alnum($word)), FILE_APPEND);	
+			// $numOfWords++;
+			// $numOfWordsInLine++;		
+		// }	
 	}
 	
 	echo 'Done Processing ' . $numOfWords . ' words<br/>';
+	
 }
 
 function getTextFileContents($fullPath) {
