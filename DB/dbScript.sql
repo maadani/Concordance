d0505fa3b9c85de75dab8e15c774f3e4b29ae572
Erez Maadani <erez.maadani@gmail.com>
2015-10-30 16:16:15 +0200

    Add SP for adding new words

--- DB/dbScript.sql
+++ DB/dbScript.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `concordancedb` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `concordancedb`;
 -- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: concordancedb
@@ -66,30 +64,6 @@ LOCK TABLES `relations` WRITE;
 /*!40000 ALTER TABLE `relations` DISABLE KEYS */;
 INSERT INTO `relations` VALUES (1,'C1','First Relation'),(2,'C2','Second Relation'),(3,'rel1','desc1'),(4,'bla_bla1','bla_bla2'),(7,'bla bla1','ssdasdasd');
 /*!40000 ALTER TABLE `relations` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `sonnet_sections`
---
-
-DROP TABLE IF EXISTS `sonnet_sections`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sonnet_sections` (
-  `section_id` int(11) NOT NULL AUTO_INCREMENT,
-  `section_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`section_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sonnet_sections`
---
-
-LOCK TABLES `sonnet_sections` WRITE;
-/*!40000 ALTER TABLE `sonnet_sections` DISABLE KEYS */;
-INSERT INTO `sonnet_sections` VALUES (1,'Ingredients'),(2,'Instructions'),(3,'Serves'),(4,'Notes');
-/*!40000 ALTER TABLE `sonnet_sections` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -186,7 +160,7 @@ CREATE TABLE `words` (
   `num_of_files_appearance` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `value_UNIQUE` (`value`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=557 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -195,7 +169,7 @@ CREATE TABLE `words` (
 
 LOCK TABLES `words` WRITE;
 /*!40000 ALTER TABLE `words` DISABLE KEYS */;
-INSERT INTO `words` VALUES (1,'a',0,0),(2,'b',0,0),(3,'ccc',0,0),(4,'d',0,0),(5,'ababab',NULL,NULL),(6,'ababab441',NULL,NULL),(8,'jsjsjs',NULL,NULL),(17,'jsjsjs2',NULL,NULL);
+INSERT INTO `words` VALUES (1,'a',0,0),(2,'b',0,0),(3,'ccc',0,0),(4,'d',0,0),(5,'ababab',NULL,NULL),(6,'ababab441',NULL,NULL),(8,'jsjsjs',NULL,NULL),(17,'jsjsjs2',NULL,NULL),(18,'1',NULL,NULL),(19,'addWordTest1',NULL,NULL),(463,'When',NULL,NULL),(464,'forty',NULL,NULL),(465,'winters',NULL,NULL),(466,'shall',NULL,NULL),(467,'beseige',NULL,NULL),(468,'thy',NULL,NULL),(469,'brow',NULL,NULL),(470,', ',NULL,NULL),(471,' ',NULL,NULL),(472,'And',NULL,NULL),(473,'dig',NULL,NULL),(474,'deep',NULL,NULL),(475,'trenches',NULL,NULL),(476,'in',NULL,NULL),(477,'beauty',NULL,NULL),(478,'\\\'',NULL,NULL),(479,'s',NULL,NULL),(480,'field',NULL,NULL),(481,'youth',NULL,NULL),(482,'proud',NULL,NULL),(483,'livery',NULL,NULL),(484,'so',NULL,NULL),(485,'gazed',NULL,NULL),(486,'on',NULL,NULL),(487,'now',NULL,NULL),(488,'Will',NULL,NULL),(489,'be',NULL,NULL),(490,'tatter',NULL,NULL),(491,'weed',NULL,NULL),(492,'of',NULL,NULL),(493,'small',NULL,NULL),(494,'worth',NULL,NULL),(495,'held',NULL,NULL),(496,': ',NULL,NULL),(497,'Then',NULL,NULL),(498,'being',NULL,NULL),(499,'ask',NULL,NULL),(500,'where',NULL,NULL),(501,'all',NULL,NULL),(502,'lies',NULL,NULL),(503,'the',NULL,NULL),(504,'treasure',NULL,NULL),(505,'lusty',NULL,NULL),(506,'days',NULL,NULL),(507,'To',NULL,NULL),(508,'say',NULL,NULL),(509,'within',NULL,NULL),(510,'thine',NULL,NULL),(511,'own',NULL,NULL),(512,'-',NULL,NULL),(513,'sunken',NULL,NULL),(514,'eyes',NULL,NULL),(515,'Were',NULL,NULL),(516,'an',NULL,NULL),(517,'eating',NULL,NULL),(518,'shame',NULL,NULL),(519,'thriftless',NULL,NULL),(520,'praise',NULL,NULL),(521,'. ',NULL,NULL),(522,'How',NULL,NULL),(523,'much',NULL,NULL),(524,'more',NULL,NULL),(525,'deserved',NULL,NULL),(526,'use',NULL,NULL),(527,'If',NULL,NULL),(528,'thou',NULL,NULL),(529,'couldst',NULL,NULL),(530,'answer',NULL,NULL),(531,'This',NULL,NULL),(532,'fair',NULL,NULL),(533,'child',NULL,NULL),(534,'mine',NULL,NULL),(535,'sum',NULL,NULL),(536,'my',NULL,NULL),(537,'count',NULL,NULL),(538,'make',NULL,NULL),(539,'old',NULL,NULL),(540,'excuse',NULL,NULL),(541,',\\\' ',NULL,NULL),(542,'Proving',NULL,NULL),(543,'his',NULL,NULL),(544,'by',NULL,NULL),(545,'succession',NULL,NULL),(546,'! ',NULL,NULL),(547,'new',NULL,NULL),(548,'made',NULL,NULL),(549,'art',NULL,NULL),(550,'see',NULL,NULL),(551,'blood',NULL,NULL),(552,'warm',NULL,NULL),(553,'feel',NULL,NULL),(554,'st',NULL,NULL),(555,'it',NULL,NULL),(556,'cold',NULL,NULL);
 /*!40000 ALTER TABLE `words` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -268,11 +242,8 @@ CREATE TABLE `words_in_sonnets` (
   `word_index_in_sonnet` int(11) NOT NULL,
   `line_index` int(11) NOT NULL,
   `word_index_in_line` int(11) NOT NULL,
-  `section_id` int(11) NOT NULL,
   PRIMARY KEY (`sonnet_id`,`word_index_in_sonnet`),
   KEY `wir_word_id_idx` (`word_id`),
-  KEY `wir_section_id_idx` (`section_id`),
-  CONSTRAINT `fk_section_id` FOREIGN KEY (`section_id`) REFERENCES `sonnet_sections` (`section_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_sonnet_id` FOREIGN KEY (`sonnet_id`) REFERENCES `sonnets` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_word_id` FOREIGN KEY (`word_id`) REFERENCES `words` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -284,7 +255,7 @@ CREATE TABLE `words_in_sonnets` (
 
 LOCK TABLES `words_in_sonnets` WRITE;
 /*!40000 ALTER TABLE `words_in_sonnets` DISABLE KEYS */;
-INSERT INTO `words_in_sonnets` VALUES (1,1,1,1,1,1),(2,1,2,2,1,1),(3,1,3,3,1,1),(4,1,4,4,1,1),(5,2,7,3,2,2),(8,2,10,10,10,2),(17,2,11,11,11,3),(17,2,12,12,12,3);
+INSERT INTO `words_in_sonnets` VALUES (1,1,1,1,1),(2,1,2,2,1),(3,1,3,3,1),(4,1,4,4,1),(463,2,1,1,1),(464,2,2,1,2),(465,2,3,1,3),(466,2,4,1,4),(467,2,5,1,5),(468,2,6,1,6),(469,2,7,1,7),(470,2,8,1,8),(471,2,9,2,1),(472,2,10,2,2),(473,2,11,2,3),(474,2,12,2,4),(475,2,13,2,5),(476,2,14,2,6),(468,2,15,2,7),(477,2,16,2,8),(478,2,17,2,9),(479,2,18,2,10),(480,2,19,2,11),(470,2,20,2,12),(471,2,21,3,1),(468,2,22,3,2),(481,2,23,3,3),(478,2,24,3,4),(479,2,25,3,5),(482,2,26,3,6),(483,2,27,3,7),(470,2,28,3,8),(484,2,29,3,9),(485,2,30,3,10),(486,2,31,3,11),(487,2,32,3,12),(470,2,33,3,13),(471,2,34,4,1),(488,2,35,4,2),(489,2,36,4,3),(1,2,37,4,4),(490,2,38,4,5),(478,2,39,4,6),(4,2,40,4,7),(491,2,41,4,8),(470,2,42,4,9),(492,2,43,4,10),(493,2,44,4,11),(494,2,45,4,12),(495,2,46,4,13),(496,2,47,4,14),(471,2,48,5,1),(497,2,49,5,2),(498,2,50,5,3),(499,2,51,5,4),(478,2,52,5,5),(4,2,53,5,6),(500,2,54,5,7),(501,2,55,5,8),(468,2,56,5,9),(477,2,57,5,10),(502,2,58,5,11),(470,2,59,5,12),(471,2,60,6,1),(500,2,61,6,2),(501,2,62,6,3),(503,2,63,6,4),(504,2,64,6,5),(492,2,65,6,6),(468,2,66,6,7),(505,2,67,6,8),(506,2,68,6,9),(470,2,69,6,10),(471,2,70,7,1),(507,2,71,7,2),(508,2,72,7,3),(470,2,73,7,4),(509,2,74,7,5),(510,2,75,7,6),(511,2,76,7,7),(474,2,77,7,8),(512,2,78,7,9),(513,2,79,7,10),(514,2,80,7,11),(470,2,81,7,12),(471,2,82,8,1),(515,2,83,8,2),(516,2,84,8,3),(501,2,85,8,4),(512,2,86,8,5),(517,2,87,8,6),(518,2,88,8,7),(472,2,89,8,8),(519,2,90,8,9),(520,2,91,8,10),(521,2,92,8,11),(471,2,93,9,1),(522,2,94,9,2),(523,2,95,9,3),(524,2,96,9,4),(520,2,97,9,5),(525,2,98,9,6),(468,2,99,9,7),(477,2,100,9,8),(478,2,101,9,9),(479,2,102,9,10),(526,2,103,9,11),(470,2,104,9,12),(471,2,105,10,1),(527,2,106,10,2),(528,2,107,10,3),(529,2,108,10,4),(530,2,109,10,5),(478,2,110,10,6),(531,2,111,10,7),(532,2,112,10,8),(533,2,113,10,9),(492,2,114,10,10),(534,2,115,10,11),(471,2,116,11,1),(466,2,117,11,2),(535,2,118,11,3),(536,2,119,11,4),(537,2,120,11,5),(472,2,121,11,6),(538,2,122,11,7),(536,2,123,11,8),(539,2,124,11,9),(540,2,125,11,10),(541,2,126,11,11),(471,2,127,12,1),(542,2,128,12,2),(543,2,129,12,3),(477,2,130,12,4),(544,2,131,12,5),(545,2,132,12,6),(510,2,133,12,7),(546,2,134,12,8),(471,2,135,13,1),(531,2,136,13,2),(515,2,137,13,3),(507,2,138,13,4),(489,2,139,13,5),(547,2,140,13,6),(548,2,141,13,7),(463,2,142,13,8),(528,2,143,13,9),(549,2,144,13,10),(539,2,145,13,11),(470,2,146,13,12),(471,2,147,14,1),(472,2,148,14,2),(550,2,149,14,3),(468,2,150,14,4),(551,2,151,14,5),(552,2,152,14,6),(463,2,153,14,7),(528,2,154,14,8),(553,2,155,14,9),(478,2,156,14,10),(554,2,157,14,11),(555,2,158,14,12),(556,2,159,14,13),(521,2,160,14,14);
 /*!40000 ALTER TABLE `words_in_sonnets` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -588,6 +559,39 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_add_word` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_add_word`(in inWord varchar(50), in inSonetId int, in inWordIndexInFile int, in inLineIndex int, in inWordInLineIndex int)
+BEGIN
+
+    DECLARE exit handler for SQLEXCEPTION
+        BEGIN
+        ROLLBACK;
+    END;
+	START TRANSACTION;
+	set @wordId = (select words.id from words where value = inWord);
+    IF(@wordId IS NULL) THEN
+		insert into words(words.value) values(inWord);
+        set @wordId = (SELECT LAST_INSERT_ID());
+    END IF;
+    
+    insert into words_in_sonnets(words_in_sonnets.word_id, words_in_sonnets.sonnet_id, words_in_sonnets.word_index_in_sonnet, words_in_sonnets.line_index, words_in_sonnets.word_index_in_line)
+    values(@wordId, inSonetId, inWordIndexInFile, inLineIndex, inWordInLineIndex);
+	COMMIT;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `sp_add_words_to_relation` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -719,4 +723,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-10-29  1:56:49
+-- Dump completed on 2015-10-30 16:15:02
