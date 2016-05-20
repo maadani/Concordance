d77de7dffa7879e2f3145c35aca0a1c2fd50bbd9
Erez Maadani <erez.maadani@gmail.com>
2016-05-20 15:19:56 +0300

    add set meta data methods

--- DB/mydbr/default/reports/sp_DBR_find_recipe.sql
+++ DB/mydbr/default/reports/sp_DBR_find_recipe.sql
@@ -3,11 +3,14 @@ drop procedure if exists `sp_DBR_find_recipe`
 $$
 CREATE PROCEDURE `sp_DBR_find_recipe`(inSonnetId int, inAuther varchar(45), inSequenceId int, inContainsText varchar(200))
 BEGIN
+/*
 
 select 'dbr.text', concat('inSonnetId: ', inSonnetId);
 select 'dbr.text', concat('inAuther: ', inAuther);
 select 'dbr.text', concat('inSequenceId: ', inSequenceId);
 select 'dbr.text', concat('inContainsText: ', inContainsText);
+*/
+
 
 call concordancedb.get_sonnet_by_params(inSonnetId, inAuther, inSequenceId, inContainsText);
 
